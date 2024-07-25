799106053e472dbb75ce79ef176d1864e09d47e9
mohammad <mohammadalmoussawiabc@gmail.com>
2024-07-25 12:21:44 +0300

    Add project files.

---
+++ ExercisesApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercisesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a console application");
+        }
+    }
+}
